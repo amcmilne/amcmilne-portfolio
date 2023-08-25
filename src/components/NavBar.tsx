2f69963a949be3b2b5531a6f239acf5f08da71aa
amcmilne <amcmilne@gmail.com>
2023-08-25 08:11:25 -0400

    change to look

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
-import { FC } from 'react'
 import { Link as ScrollLink } from 'react-scroll'
 import content from '../content/config'
 
-const NavBar: FC = () => {
+const NavBar = () => {
   return (
     <div
       style={{
@@ -11,19 +10,6 @@ const NavBar: FC = () => {
       className={`font-display fixed top-0 w-screen z-40`}
     >
       <div className={`flex items-center w-10/12 mx-auto py-3`}>
-        <ScrollLink
-          to="myrepos"
-          smooth={true}
-          className={`text-lg font-bold text-blue-steele flex-1 cursor-pointer`}
-        >
-          <h1>
-            {content.nav.logo}
-            <span
-              className={`h-3 w-3 bg-black-500 inline-block ml-2 rounded-full`}
-            ></span>
-          </h1>
-        </ScrollLink>
-
         <div className={`text-blue-steele text-xl`}>
           {content.nav.links.map((link, index) => {
             return (
